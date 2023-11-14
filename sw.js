786dfeb3286190c47fd216ad62f955b89dcf641c
luisgar289 <1720110205@utectulancingo.edu.mx>
2023-11-14 16:15:36 -0600

    Notificacion

--- sw.js
+++ sw.js
@@ -37,3 +37,12 @@ self.addEventListener("fetch", event =>  {
     event.respondWith(returnCachedResource());
 });
 
+if (navigator.setAppBadge) {
+  console.log("The App Badging API is supported!");
+}
+
+navigator.setAppBadge(64).then(() => {
+console.log("The badge was added");
+}).catch(e => {
+console.error("Error displaying the badge", e);
+});
