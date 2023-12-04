01cece96b4c0bce3672834006b3172eb6abf5b47
luisgar289 <1720110205@utectulancingo.edu.mx>
2023-12-03 19:27:32 -0600

    Push Notifications

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const guardarDatos = {
+/*const guardarDatos = {
     nombre: "Luis",
     edad: 22,
     correo: null,
@@ -26,4 +26,134 @@ navigator.setAppBadge(42).then(() => {
   console.log("The badge was added");
 }).catch(e => {
   console.error("Error displaying the badge", e);
-});
+});*/
+
+//Push Notifications
+
+const applicationServerPublicKey = 'BC_Cl2w7p7FWty_GxlWeJPBS4I88uGTKq3dp9m85adPqXGIqIK6BB2w71jPknlYvIs1tt05WlEB3nCDuXO2hVs4';
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+function updateBtn() {
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+  pushButton.disabled = false;
+}
+
+navigator.serviceWorker.register('./sw.js')
+.then(function(swReg) {
+  console.log('Service Worker is registered', swReg);
+
+  swRegistration = swReg;
+  initializeUI();
+})
+
+function updateSubscriptionOnServer(subscription) {
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    console.log(subscriptionJson.textContent);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed');
+
+    updateSubscriptionOnServer(subscription);
+
+    isSubscribed = true;
+
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn();
+  });
+}
+
+function unsubscribeUser() {
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      return subscription.unsubscribe();
+    }
+  })
+  .catch(function(error) {
+    console.log('Error unsubscribing', error);
+  })
+  .then(function() {
+    updateSubscriptionOnServer(null);
+
+    console.log('User is unsubscribed.');
+    isSubscribed = false;
+
+    updateBtn();
+  });
+}
+
+function initializeUI() {
+  pushButton.addEventListener('click', function() {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      unsubscribeUser();
+    } else {
+      subscribeUser();
+    }
+  });
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push are supported');
+
+  navigator.serviceWorker.register('./sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
--- sw.js
+++ sw.js
@@ -36,3 +36,27 @@ self.addEventListener("fetch", event =>  {
     }
     event.respondWith(returnCachedResource());
 });
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Desenigma';
+  const options = {
+    body: event.data.text(),
+    icon: './img/icon-512x512.png',
+    badge: './img/icon-512x512.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web')
+  );
+});
